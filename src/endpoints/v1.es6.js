326da48a4b40999a5f637661560e5f6f38366da1
Jack Lawson <ajacksified@gmail.com>
2014-12-08 19:15:00 -0800

    Super simple LRU cache for endpoints

--- src/endpoints/v1.es6.js
+++ src/endpoints/v1.es6.js
@@ -1,5 +1,6 @@
 import * as superagent from 'superagent';
 import * as q from 'q';
+import * as querystring from 'querystring';
 
 import Account from '../models/account';
 import Comment from '../models/comment';
@@ -7,7 +8,14 @@ import Link from '../models/link';
 import Vote from '../models/vote';
 
 
-function baseGet(uri, options, request, formatBody) {
+import * as LRU from 'lru-cache';
+
+var defaultCacheConfig = {
+ max: 500,
+ maxAge: 1000 * 50 * 3,
+}
+
+function baseGet(cache, uri, options, request, formatBody) {
   var options = options || {};
   var defer = q.defer();
 
@@ -15,6 +23,20 @@ function baseGet(uri, options, request, formatBody) {
   var headers = options.headers || {};
 
   headers['User-Agent'] = options.userAgent;
+
+  var key = uri + '?' + querystring.stringify(query);
+
+  if (headers.Authorization) {
+    cache = cache.authed;
+    key += '&auth=' + headers.Authorization;
+  } else {
+    cache = cache.unauthed;
+  }
+
+  if(cache.get(key)) {
+    defer.resolve(cache.get(key));
+    return defer.promise;
+  }
 
   request.get(uri)
     .set(headers)
@@ -31,6 +53,7 @@ function baseGet(uri, options, request, formatBody) {
           body = formatBody(body);
         }
 
+        cache.set(key, body);
         defer.resolve(body);
       } catch (e) {
         defer.reject(e);
@@ -47,7 +70,9 @@ function basePost(uri, options, request, formatBody) {
   var form = options.form || {};
   var headers = options.headers || {};
 
-  headers['User-Agent'] = options.userAgent;
+  if (options.userAgent) {
+    headers['User-Agent'] = options.userAgent;
+  }
 
   request.post(uri)
     .set(headers)
@@ -89,11 +114,41 @@ class APIv1Endpoint {
     this.origin = config.origin || 'https://www.reddit.com';
     this.request = config.request || superagent;
     this.userAgent = config.userAgent || 'SNOODE UNREGISTERED v0.0.2';
+
+    this.cache = {
+      links: {
+        unauthed: new LRU(defaultCacheConfig),
+        authed: new LRU(defaultCacheConfig),
+      },
+      comments: {
+        unauthed: new LRU(defaultCacheConfig),
+        authed: new LRU(defaultCacheConfig),
+      },
+      users: {
+        unauthed: new LRU(defaultCacheConfig),
+        authed: new LRU(defaultCacheConfig),
+      },
+    }
+  }
+
+  hydrate (endpoint, options, data) {
+    var cache = this.cache[endpoint];
+    var { uri, options } = this[endpoint].buildOptions(options);
+
+    var key = uri + '?' + querystring.stringify(options.query);
+
+    if (options.headers.Authorization) {
+      cache = cache.authed;
+    } else {
+      cache = cache.unauthed;
+    }
+
+    cache.set(key, data);
   }
 
   get links () {
     return bind({
-      get: function(options = {}) {
+      buildOptions: function(options) {
         var uri = this.origin + '/';
         var sort = options.sort || 'hot';
 
@@ -103,9 +158,17 @@ class APIv1Endpoint {
 
         uri += sort + '.json';
 
-        options.userAgent = this.userAgent;
+        if (this.userAgent) {
+          options.userAgent = this.userAgent;
+        }
 
-        return baseGet(uri, options, this.request, (body) => {
+        return { uri, options }
+      },
+
+      get: function(options = {}) {
+        var { uri, options } = this.links.buildOptions(options);
+
+        return baseGet(this.cache.links, uri, options, this.request, (body) => {
           if (body.data && body.data.children) {
             return body.data.children.map(c => new Link(c.data).toJSON());
           }else {
@@ -130,16 +193,24 @@ class APIv1Endpoint {
     }
 
     return bind({
-      get: function(options = {}) {
+      buildOptions: function(options) {
         var uri = this.origin + '/comments/' + options.linkId + '.json';
 
-        options.userAgent = this.userAgent;
+        if (this.userAgent) {
+          options.userAgent = this.userAgent;
+        }
 
         if (options.coment) {
           options.query.comment = options.comment;
         }
 
-        return baseGet(uri, options, this.request, (body) => {
+        return { uri, options }
+      },
+
+      get: function(options = {}) {
+        var { uri, options } = this.comments.buildOptions(options);
+
+        return baseGet(this.cache.comments, uri, options, this.request, (body) => {
           return {
             listing: new Link(body[0].data.children[0].data).toJSON(),
             comments: body[1].data.children.map(mapReplies)
@@ -151,7 +222,7 @@ class APIv1Endpoint {
 
   get users () {
     return bind({
-      get: function(options = {}) {
+      buildOptions: function(options) {
         var uri = this.origin + '/';
 
         if (options.user === 'me') { // current oauth doesn't return user id
@@ -160,9 +231,17 @@ class APIv1Endpoint {
           uri += 'user/' + options.user + '/about.json';
         }
 
-        options.userAgent = this.userAgent;
+        if (this.userAgent) {
+          options.userAgent = this.userAgent;
+        }
 
-        return baseGet(uri, options, this.request, (body) => {
+        return { uri, options }
+      },
+
+      get: function(options = {}) {
+        var { uri, options } = this.users.buildOptions(options);
+
+        return baseGet(this.cache.users, uri, options, this.request, (body) => {
           if (body) {
             return new Account(body).toJSON();
           }else {
