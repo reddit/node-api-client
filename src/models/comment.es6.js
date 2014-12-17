084aa8fbc5d76c7b3df8e280a439945fd6356f11
Jack Lawson <ajacksified@gmail.com>
2014-12-16 16:55:35 -0800

    Process markdown at toJSON time, v0.1.6

--- src/models/comment.es6.js
+++ src/models/comment.es6.js
@@ -1,8 +1,21 @@
 import Base from './base';
+import * as process from 'reddit-text-js';
 
 class Comment extends Base {
   constructor(props) {
     return super(props);
+  }
+
+  get bodyHtml () {
+    return process(this.get('body'));
+  }
+
+  toJSON () {
+    var props = this.props;
+
+    props.bodyHtml = this.bodyHtml;
+
+    return props;
   }
 };
 
