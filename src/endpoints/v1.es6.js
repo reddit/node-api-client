fdd2bdd7f63b60626f2dcc1e747e19c57ec2b773
Jack Lawson <ajacksified@gmail.com>
2015-03-23 12:03:21 -0700

    Fix user profile data; v0.3.6

--- src/endpoints/v1.es6.js
+++ src/endpoints/v1.es6.js
@@ -309,7 +309,7 @@ class APIv1Endpoint {
 
         return baseGet(this.cache.users, uri, options, this.request, (body) => {
           if (body) {
-            return new Account(body).toJSON();
+            return new Account(body.data).toJSON();
           }else {
             return null;
           }
