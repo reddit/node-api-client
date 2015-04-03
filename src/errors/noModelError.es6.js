67b6853c464547314b0c9cf9268c303161edd1c8
Jack Lawson <ajacksified@gmail.com>
2015-04-03 15:59:38 -0700

    Upgrade babel; v1.1.0

--- src/errors/noModelError.es6.js
+++ src/errors/noModelError.es6.js
@@ -1,5 +1,7 @@
 class NoModelError extends Error {
   constructor (endpoint) {
+    super(endpoint);
+
     this.name = 'NoModelError';
     this.message = 'No model given for api endpoint ' + endpoint;
     this.status = 400;
--- src/errors/validationError.es6.js
+++ src/errors/validationError.es6.js
@@ -1,5 +1,7 @@
 class ValidationError extends Error {
   constructor (name, model, errors) {
+    super(name);
+
     this.name = 'NoModelError';
 
     this.message = name + ' had errors in: ' + errors.join(',') +
