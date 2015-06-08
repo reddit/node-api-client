453d62f83b379ff58a2df10bf27c0c1fbcf4e7ee
David Wick <wick.d.spencer@gmail.com>
2015-06-08 12:05:00 -0700

    Add `thingId` validator

--- src/models/account.es6.js
+++ src/models/account.es6.js
@@ -4,6 +4,16 @@ class Account extends Base {
   constructor(props) {
     props._type = 'Account';
     super(props);
+
+    var account = this;
+
+    this.validators = {
+      thingId: function() {
+        var thingId = this.get('thingId');
+
+        return Base.validators.thingId(thingId);
+      }.bind(account),
+    };
   }
 };
 
