8edd2dbadad4dd88859afdedca7446efc53d5f4f
Jack Lawson <ajacksified@gmail.com>
2016-03-08 15:37:54 -0800

    Subreddit PATCH and POST and PUT

--- apis/subreddits.es6.js
+++ apis/subreddits.es6.js
@@ -1,5 +1,37 @@
+import pick from 'lodash/object/pick';
+
 import BaseAPI from './base';
 import Subreddit from '../models/subreddit';
+
+const DEFAULT_SUBREDDIT_OPTIONS = {
+  allow_top: true,
+  collapse_deleted_comments: false,
+  comment_score_hide_mins: 0,
+  description: '',
+  exclude_banned_modqueue: false,
+  'header-title': '',
+  hide_ads: false,
+  lang: 'en',
+  link_type: 'any',
+  name: '',
+  over_18: false,
+  public_description: '',
+  public_traffic: false,
+  show_media: true,
+  spam_comments: 'low',
+  spam_links: 'high',
+  spam_selfposts: 'high',
+  sr: '',
+  submit_link_label: '',
+  submit_text: '',
+  submit_text_label: '',
+  suggested_comment_sort: 'confidence',
+  title: '',
+  type: 'public',
+  wiki_edit_age: 0,
+  wiki_edit_karma: 100,
+  wikimode: 'disabled',
+};
 
 class Subreddits extends BaseAPI {
   static dataCacheConfig = {
@@ -19,9 +51,59 @@ class Subreddits extends BaseAPI {
   model = Subreddit;
 
   del = this.notImplemented('del');
-  post = this.notImplemented('post');
-  put = this.notImplemented('put');
-  patch = this.notImplemented('patch');
+
+  patch (data) {
+    const post = this.post.bind(this);
+    const get = this.get.bind(this);
+    // If the data doesn't have all of the keys, get the full subreddit data
+    // and then merge in the changes and submit _that_. The API requires the
+    // full object be sent.
+    if (Object.keys(data).sort() !== Subreddit.fields) {
+      return new Promise((r, x) => {
+        get({
+          id: data.id,
+          view: 'mod',
+        }).then(function(sub) {
+          const postData = pick({
+            ...DEFAULT_SUBREDDIT_OPTIONS,
+            ...sub,
+            ...data,
+            sr: sub.name,
+          }, Object.keys(DEFAULT_SUBREDDIT_OPTIONS));
+
+          console.log(postData);
+
+          return post(postData);
+        }, x);
+      });
+    }
+
+    return super.post(data);
+  }
+
+  get (query) {
+    delete query.view;
+    return super.get(query);
+  }
+
+  post (data) {
+    const postData = pick({
+      ...DEFAULT_SUBREDDIT_OPTIONS,
+      ...data,
+    }, Object.keys(DEFAULT_SUBREDDIT_OPTIONS));
+
+    return super.post(postData);
+  }
+
+  put (data) {
+    const postData = pick({
+      ...DEFAULT_SUBREDDIT_OPTIONS,
+      ...data,
+      name: data.id,
+    }, Object.keys(DEFAULT_SUBREDDIT_OPTIONS));
+
+    return super.post(postData);
+  }
 
   formatCacheData = (data) => {
     const api = this.api;
@@ -44,6 +126,14 @@ class Subreddits extends BaseAPI {
     };
   }
 
+  formatQuery (query, method) {
+    if (method !== 'get') {
+      query.api_type = 'json';
+    }
+
+    return query;
+  }
+
   formatBody (res) {
     const { body } = res;
 
@@ -57,11 +147,19 @@ class Subreddits extends BaseAPI {
   }
 
   path (method, query={}) {
-    if (query.id) {
-      return `r/${query.id}/about.json`;
+    if (method === 'get') {
+      if (query.id && query.view === 'mod') {
+        return `r/${query.id}/about/edit.json`;
+      }
+
+      if (query.id) {
+        return `r/${query.id}/about.json`;
+      }
+
+      return `subreddits/${query.sort || 'default'}.json`;
     }
 
-    return `subreddits/${query.sort || 'default'}.json`;
+    return 'api/site_admin';
   }
 }
 
--- models/subreddit.es6.js
+++ models/subreddit.es6.js
@@ -1,7 +1,41 @@
 import Base from './base';
 
+const EDIT_FIELDS = [
+  'default_set',
+  'subreddit_id',
+  'domain',
+  'show_media',
+  'wiki_edit_age',
+  'submit_text',
+  'spam_links',
+  'title',
+  'collapse_deleted_comments',
+  'wikimode',
+  'over_18',
+  'related_subreddits',
+  'suggested_comment_sort',
+  'description',
+  'submit_link_label',
+  'spam_comments',
+  'spam_selfposts',
+  'submit_text_label',
+  'key_color',
+  'language',
+  'wiki_edit_karma',
+  'hide_ads',
+  'header_hover_text',
+  'public_traffic',
+  'public_description',
+  'comment_score_hide_mins',
+  'subreddit_type',
+  'exclude_banned_modqueue',
+  'content_options',
+].sort();
+
 class Subreddit extends Base {
   _type = 'Subreddit';
+
+  static fields = EDIT_FIELDS;
 
   constructor(props) {
     delete props.submit_text_html;
