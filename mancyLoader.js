1724978d8321b9287546e35c8849fad7d7a77baa
Ryan Schwers <schwers.r@gmail.com>
2016-03-08 12:35:08 -0800

    Add a script to facilitate loading into Mancy

---
+++ mancyLoader.js
@@ -0,0 +1,18 @@
+require('babel-register')({
+  ignore: false,
+  only: /.+(?:(?:\.es6\.js)|(?:.jsx))$/,
+  extensions: ['.js', '.es6.js'],
+  sourceMap: true,
+  presets: [
+    'es2015',
+  ],
+  plugins: [
+    'transform-object-rest-spread',
+    'transform-async-to-generator',
+    'transform-class-properties',
+    'syntax-trailing-function-commas',
+  ],
+});
+
+const Snoode = require('./index').default;
+module.exports = new Snoode();
