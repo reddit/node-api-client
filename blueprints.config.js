5187c57100db24486efc4f4119d0bec382d39a48
Niranjan Ramadas <nrbramadas@gmail.com>
2016-04-26 13:54:43 -0700

    Add @r/build

---
+++ blueprints.config.js
@@ -0,0 +1,33 @@
+module.exports = [{
+  name: 'apiClient',
+  webpack: {
+    entry: './index.es6.js',
+    output: {
+      generator: 'simple',
+      dest: './',
+    },
+    resolve: {
+      generator: 'npm-and-modules',
+      extensions: ['', '.js', '.jsx', '.es6.js', '.json'],
+    },
+    loaders: [
+      'esnextreact',
+      'json',
+    ],
+    plugins: [
+      'production-loaders',
+      'set-node-env',
+      'minify-and-treeshake',
+      'abort-if-errors',
+    ],
+    externals: {
+      lodash: 'commonjs lodash',
+      'lodash/object': 'commonjs lodash/object',
+      'lodash/lang': 'commonjs lodash/lang',
+      'lodash/array': 'commonjs lodash/array',
+      'lodash/collection': 'commonjs lodash/collection',
+      superagent: 'commonjs superagent',
+      'superagent-retry': 'commonjs superagent-retry',
+    },
+  }
+}]
