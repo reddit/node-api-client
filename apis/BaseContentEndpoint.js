7a3d682ac0551e852ffe6438c86c431d11a2e6af
Ryan Schwers <schwers.r@gmail.com>
2016-05-17 16:13:47 -0700

    Rip out BaseEndpoint, wasn't being used anyway

--- apis/BaseContentEndpoint.js
+++ apis/BaseContentEndpoint.js
@@ -1,5 +1,4 @@
 import { pick } from 'lodash/object';
-import BaseEndpoint from '../apiBase/BaseEndpoint';
 import NoModelError from '../apiBase/errors/NoModelError';
 
 const MOD_ACTION_MAP = {
@@ -48,80 +47,49 @@ export const formatBaseContentQuery = (query, method) => {
   return query;
 };
 
-export default class BaseContentEndpoint extends BaseEndpoint {
-  static move = BaseEndpoint.notImplemented('move');
-  static copy = BaseEndpoint.notImplemented('copy');
-  static put = BaseEndpoint.notImplemented('put');
+export const  patchPath = () => {
+  return 'api/editusertext';
+};
 
-  static formatQuery(query, method) {
-    if (method !== 'patch') {
-      query.feature = 'link_preview';
-      query.sr_detail = 'true';
-    }
+export const  deletePath = () => {
+  return 'api/del';
+};
 
-    if (method === 'del') {
-      query._method = 'post';
-    }
-
-    return query;
+export const  patch = (apiOptions, data) => {
+  if (!data) {
+    throw new NoModelError('/api/editusertext');
   }
 
-  static path(method, query={}) {
-    if (method === 'get') {
-      return this.getPath(query);
-    } else if (method === 'post') {
-      return this.postPath(query);
-    } else if (method === 'patch') {
-      return this.patchPath(query);
-    } else if (method === 'del') {
-      return this.deletePath(query);
+  const promises = [];
+
+  Object.keys(data).map(k => {
+    const prop = MOD_ACTION_MAP[k];
+    const val = data[k];
+
+    if (prop) {
+      const [api, json] = prop(val, data);
+      promises.push(new Promise((r, x) => {
+        this.rawSend('post', api, json, (err, res, req) => {
+          if (err || !res.ok) {
+            x(err || res);
+          }
+
+          r(res, req);
+        });
+      }));
     }
+  });
+
+  if (data.text) {
+    const json = {
+      api_type: 'json',
+      thing_id: data.id,
+      text: data.text,
+      _method: 'post',
+    };
+
+    promises.push(this.save('patch', json));
   }
 
-  static patchPath () {
-    return 'api/editusertext';
-  }
-
-  static deletePath () {
-    return 'api/del';
-  }
-
-  static patch = (apiOptions, data) => {
-    if (!data) {
-      throw new NoModelError('/api/editusertext');
-    }
-
-    const promises = [];
-
-    Object.keys(data).map(k => {
-      const prop = MOD_ACTION_MAP[k];
-      const val = data[k];
-
-      if (prop) {
-        const [api, json] = prop(val, data);
-        promises.push(new Promise((r, x) => {
-          this.rawSend('post', api, json, (err, res, req) => {
-            if (err || !res.ok) {
-              x(err || res);
-            }
-
-            r(res, req);
-          });
-        }));
-      }
-    });
-
-    if (data.text) {
-      const json = {
-        api_type: 'json',
-        thing_id: data.id,
-        text: data.text,
-        _method: 'post',
-      };
-
-      promises.push(this.save('patch', json));
-    }
-
-    return Promise.all(promises);
-  }
-}
+  return Promise.all(promises);
+};
