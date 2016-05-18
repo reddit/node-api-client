7c11cf38d096106af67c1a2dae4c35f48bac5468
Ryan Schwers <schwers.r@gmail.com>
2016-05-18 15:08:51 -0700

    Fetched logged out user data

--- apis/accounts.es6.js
+++ apis/accounts.es6.js
@@ -1,9 +1,10 @@
 import { runQuery } from '../apiBase/APIRequestUtils';
 import Account from '../models2/account';
 
-
 const getPath = (query) => {
-  if (query.user === 'me') {
+  if (query.loggedOut) {
+    return 'api/me.json';
+  } else if (query.user === 'me') {
     return 'api/v1/me';
   } else {
     return `user/${query.user}/about.json`;
@@ -13,7 +14,12 @@ const getPath = (query) => {
 const parseGetBody = (res, apiResponse) => {
   const { body } = res;
   if (body) {
-    apiResponse.addResult(Account.fromJSON(body.data || body));
+    const data = {
+      name: 'me', // me is reserved, this should only stay me in the logged out case
+      ...(body.data || body),
+    };
+
+    apiResponse.addResult(Account.fromJSON(data));
   }
 };
 
