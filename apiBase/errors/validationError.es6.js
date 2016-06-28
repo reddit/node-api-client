063ce53e592378f6de74fae528f3e26e1c24cbe9
Niranjan Ramadas <nrbramadas@gmail.com>
2016-06-28 16:27:00 -0700

    Fixups to the save endpoint

--- apiBase/errors/validationError.es6.js
+++ apiBase/errors/validationError.es6.js
@@ -1,13 +1,19 @@
 import FakeError from './FakeError';
 
+const msgText = (apiName, errors, model) =>
+  `${ apiName } had errors in ${ errors.join(',') } with properties ${ JSON.stringify(model.toJSON()) }`;
+
 export default class ValidationError extends FakeError {
-  constructor (name, model, errors) {
-    super(name);
+  constructor (apiName, model, errors) {
+    super(apiName);
 
-    this.name = 'NoModelError';
+    this.name = 'ValidationError';
 
-    this.message = name + ' had errors in: ' + errors.join(',') +
-                    ' with properties ' + JSON.stringify(model.toJSON());
+    if (errors && errors.length && model) {
+      this.message = msgText(apiName, errors, model);
+    } else {
+      this.message = `Validation error in '${ apiName }'`;
+    }
 
     this.status = 422;
   }
