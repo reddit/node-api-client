cbe373f140d51c89c44fc0e487b0b0d6a150b270
Joshua Uziel <uzi@reddit.com>
2016-10-05 23:16:40 -0700

    Add support for posting message replies
    Converted to apiRequest based and added
    pagination support as well.

--- src/apis/MessagesEndpoint.es6.js
+++ src/apis/MessagesEndpoint.es6.js
@@ -1,7 +1,11 @@
+import { has } from 'lodash/object';
+
+import apiRequest from '../apiBase/apiRequest';
+import ValidationError from '../apiBase/errors/ValidationError';
+
 import Comment from '../models2/CommentModel';
 import Post from '../models2/PostModel';
 import Message from '../models2/MessageModel';
-import { runQuery, runForm } from '../apiBase/APIRequestUtils';
 
 const TYPE_MAP = {
   't1': Comment,
@@ -9,8 +13,8 @@ const TYPE_MAP = {
   't4': Message,
 };
 
-const parseGetBody = (res, apiResponse, apiRequest) => {
-  const { body } = res;
+const parseGetBody = apiResponse => {
+  const { body } = apiResponse.response;
   body.data.children.forEach(c => {
     const replies = [];
     if (c.data.replies) {
@@ -23,15 +27,33 @@ const parseGetBody = (res, apiResponse, apiRequest) => {
     c.data.replies = replies;
     apiResponse.addResult(TYPE_MAP[c.kind].fromJSON(c.data));
   });
+
+  const { before, after } = body.data;
+  apiResponse.meta = { before, after };
+
+  return apiResponse;
 };
 
-const parsePostBody = (res, apiResponse, apiRequest) => {
-  const { body } = res;
-  console.log(body);
+const parsePostBody = apiResponse => {
+  const { body } = apiResponse.response;
+  if (has(body, 'json.errors') && body.json.errors.length) {
+    // There's a problem -- return the errors
+    const reqType = (apiResponse.request.url.indexOf('compose') !== -1) ? 'compose' : 'reply';
+    throw new ValidationError(`Message ${reqType}`, body.json.errors, body.status);
+  } else if (has(body, 'json.data.things.0.data')) {
+    // We're replying to an existing thread -- the API gives us
+    // that message, so we return the model.
+    const message = body.json.data.things[0];
+    const model = TYPE_MAP[message.kind].fromJSON(message.data);
+    return model;
+  }
+
+  // New message thread -- API tells us nothing, so we return null
+  return null;
 };
 
-const getPath = (query={}) => {
-  const { subreddit, type, thread } = query;
+const getPath = (data={}) => {
+  const { subreddit, type, thread } = data;
   const sub = subreddit ? `r/${subreddit}/` : '';
   const id = thread ? `/${thread}` : '';
   return `${sub}message/${type}${id}.json`;
@@ -39,28 +61,49 @@ const getPath = (query={}) => {
 
 const postPath = (body={}) => {
   const { thingId } = body;
+  if (!thingId) {
+    return 'api/compose';
+  }
 
-  if (!thingId) { return 'api/compose'; }
-
-  // `api/comment` is intentional; message replies are treated as
-  // comments.
+  // `api/comment` is intentional; message replies are treated as comments.
   return 'api/comment';
 };
 
-const translateData = data => ({
-  api_type: 'json',
-  to: data.to,
-  from_sr: data.subreddit,
-  subject: data.subject,
-  text: data.body,
-});
+const translateData = data => {
+  const ret = {
+    api_type: 'json',
+    raw_json: 1,
+    text: data.body,
+  };
+
+  if (data.thingId) {
+    ret.thing_id = data.thingId;
+  }
+  if (data.to) {
+    ret.to = data.to;
+  }
+  if (data.subreddit) {
+    ret.from_sr = data.subreddit;
+  }
+  if (data.subject) {
+    ret.subject = data.subject;
+  }
+
+  return ret;
+};
 
 export default {
-  get(apiOptions, query) {
-    const { type } = query;
-    return runQuery(apiOptions, 'get', getPath(query), {}, query, parseGetBody);
+  get(apiOptions, data) {
+    const path = getPath(data);
+    const query = { ...data.query, raw_json: 1 };
+
+    return apiRequest(apiOptions, 'GET', path, { query }).then(parseGetBody);
   },
-  post(apiOptions, body) {
-    return runForm(apiOptions, 'post', postPath(body), translateData(body), parsePostBody);
+
+  post(apiOptions, data) {
+    const path = postPath(data);
+    const body = translateData(data);
+
+    return apiRequest(apiOptions, 'POST', path, { body, type: 'form' }).then(parsePostBody);
   },
 };
