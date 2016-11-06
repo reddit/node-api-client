469a03ce701c4a73c8e23ca69110c1b0bd6c64d3
Phil Aquilina <philaquilina@gmail.com>
2016-11-05 22:49:08 -0700

    Fix bug in unredditify

--- src/lib/unredditifyLink.es6.js
+++ src/lib/unredditifyLink.es6.js
@@ -1,4 +1,4 @@
 export default function unredditifyLink(url) {
   if (!url) { return; }
-  return url.replace(/^https?:\/\/(?:www\.)?reddit.com/, '');
+  return url.replace(/^https?:\/\/(?:np\.)?(?:www\.)?reddit\.com\//, '/');
 }
