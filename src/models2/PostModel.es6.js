185f290b12925b71d8a340423e5fc7fa8e4f68a0
birakattack <jesjit.birak@reddit.com>
2017-01-11 13:54:00 -0800

    Ignore spoiler thumbnails

--- src/models2/PostModel.es6.js
+++ src/models2/PostModel.es6.js
@@ -5,7 +5,7 @@ import replyable from './mixins/replyable';
 
 const T = RedditModel.Types;
 
-const IGNORED_THUMBNAILS = new Set(['default', 'image', 'self', 'nsfw']);
+const IGNORED_THUMBNAILS = new Set(['default', 'image', 'self', 'nsfw', 'spoiler']);
 const cleanThumbnail = thumbnail => {
   return IGNORED_THUMBNAILS.has(thumbnail) ? '' : thumbnail;
 };
