7efcf0ae25934384e277d57973f8fe09cdebabc3
Matt Lee <matt@reddit.com>
2017-01-27 12:06:35 -0500

    Add setSubredditSticky to api/modTools

--- src/apis/modTools.es6.js
+++ src/apis/modTools.es6.js
@@ -37,4 +37,27 @@ const distinguish = (apiOptions, fullname, distinguishType) => {
   return apiRequest(apiOptions, 'POST', 'api/distinguish', { body, type: 'form' });
 }
 
-export default { remove, approve, distinguish, DISTINGUISH_TYPES }
+/**
+ * Set or unset a stickied post (AKA an "Annoucement").
+ * See also: https://www.reddit.com/dev/api#POST_api_set_subreddit_sticky
+ * @function
+ * @param {Object} apiOptions
+ * @param {string} fullname The fullname of the target post
+ * @param {boolean} isStickied Whether to sticky or unsticky the post
+ * @param {?number} [stickyNum] Allows for specifying the "slot" to sticky the post
+ *      into, or for specifying which post to unsticky.
+ */
+const setSubredditSticky = (apiOptions, fullname, isStickied, stickyNum=null) => {
+  const body = {
+    id: fullname,
+    state: isStickied,
+  };
+
+  if (stickyNum) {
+    body.num = stickyNum;
+  }
+
+  return apiRequest(apiOptions, 'POST', 'api/set_subreddit_sticky', { body, type: 'form' });
+}
+
+export default { remove, approve, distinguish, setSubredditSticky, DISTINGUISH_TYPES }
