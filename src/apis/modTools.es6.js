7c52cd50865354ca2ff34f5afb5710f904e40fbd
scarow <samantha.carow@gmail.com>
2017-01-26 16:55:43 -0800

    Add distingiush endpoint to modtools

--- src/apis/modTools.es6.js
+++ src/apis/modTools.es6.js
@@ -1,19 +1,34 @@
 import apiRequest from '../apiBase/apiRequest';
 
-const remove = (apiOptions, fullname, spam) => {
-    // Remove a link, comment, or modmail message.
-    const body = {
-    	id: fullname,
-    	spam: spam
-    };
+const DISTINGUISH_TYPES = {
+  NONE: '',
+  MODERATOR: 'moderator',
+  ADMIN: 'admin',
+};
 
-    return apiRequest(apiOptions, 'POST', 'api/remove', { body, type: 'form' });
+const remove = (apiOptions, fullname, spam) => {
+  // Remove a link, comment, or modmail message.
+  const body = {
+    id: fullname,
+    spam: spam
+  };
+
+  return apiRequest(apiOptions, 'POST', 'api/remove', { body, type: 'form' });
 }
 
 const approve = (apiOptions, fullname) => {
-    // Approve a link or comment
-    const body = { id: fullname };
-    return apiRequest(apiOptions, 'POST', 'api/approve', { body, type: 'form' });
+  // Approve a link or comment
+  const body = { id: fullname };
+  return apiRequest(apiOptions, 'POST', 'api/approve', { body, type: 'form' });
 }
 
-export default { remove, approve }
+const distinguish = (apiOptions, fullname, distinguishType) => {
+  // Distinguish a link or comment
+  const body = {
+    how: distinguishType,
+    id: fullname,
+  };
+  return apiRequest(apiOptions, 'POST', 'api/distinguish', { body, type: 'form' });
+}
+
+export default { remove, approve, distinguish, DISTINGUISH_TYPES }
