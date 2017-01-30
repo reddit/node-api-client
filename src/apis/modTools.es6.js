926bd811a0fa2732a1b88645f41ee22a8f0fe6ae
Matt Lee <matt@reddit.com>
2017-01-30 14:47:53 -0500

    Add setStickyComment to api/modTools

--- src/apis/modTools.es6.js
+++ src/apis/modTools.es6.js
@@ -1,5 +1,11 @@
 import apiRequest from '../apiBase/apiRequest';
 
+/**
+ * Valid distinguish types.
+ * Note that the API endpoint used to distinguish posts and comments accepts
+ * 'yes' instead of 'moderator' and 'no' instead of ''.  See #distinguish
+ * @enum
+ */
 const DISTINGUISH_TYPES = {
   NONE: '',
   MODERATOR: 'moderator',
@@ -22,19 +28,30 @@ const approve = (apiOptions, fullname) => {
   return apiRequest(apiOptions, 'POST', 'api/approve', { body, type: 'form' });
 }
 
-const distinguish = (apiOptions, fullname, distinguishType) => {
-  // Distinguish a link or comment
-
+/**
+ * Distinguish a link or comment
+ * @function
+ * @param {Object} apiOptions
+ * @param {string} fullname The fullname of the target comment
+ * @param {DISTINGUISH_TYPES} distinguishType What type of distinguish is being set
+ * @param {?bool} [_sticky] For internal use by #setStickyComment
+ */
+const distinguish = (apiOptions, fullname, distinguishType, _sticky=null) => {
   const distinguishTypeMap = {
-    'moderator': 'yes',
-    '': 'no',
+    [DISTINGUISH_TYPES.MODERATOR]: 'yes',
+    [DISTINGUISH_TYPES.NONE]: 'no',
   };
 
   const body = {
-    how: distinguishTypeMap[distinguishType],
     id: fullname,
   };
-  return apiRequest(apiOptions, 'POST', 'api/distinguish', { body, type: 'form' });
+  const how = distinguishTypeMap[distinguishType] || distinguishType;
+
+  if (_sticky !== null) {
+    body.sticky = _sticky;
+  }
+
+  return apiRequest(apiOptions, 'POST', `api/distinguish/${how}`, { body, type: 'form' });
 }
 
 /**
@@ -60,4 +77,27 @@ const setSubredditSticky = (apiOptions, fullname, isStickied, stickyNum=null) =>
   return apiRequest(apiOptions, 'POST', 'api/set_subreddit_sticky', { body, type: 'form' });
 }
 
-export default { remove, approve, distinguish, setSubredditSticky, DISTINGUISH_TYPES }
+/**
+ * Sticky or unsticky a comment.
+ * Sticky comments are a special case of distinguished comments, and are done
+ * through the same API endpoint (api/distinguish).  That endpoint also handles
+ * distinguishing posts, but it does *not* handle sticky posts.  To avoid
+ * confusion, we'll keep sticky comments separated here.
+ * @function
+ * @param {Object} apiOptions
+ * @param {string} fullname The fullname of the target comment
+ * @param {boolean} isStickied Whether to sticky or unsticky the comment
+ */
+const setStickyComment = (apiOptions, fullname, isStickied) => {
+  const distinguishType = isStickied ? DISTINGUISH_TYPES.MODERATOR : DISTINGUISH_TYPES.NONE;
+  return distinguish(apiOptions, fullname, distinguishType, isStickied);
+};
+
+export default {
+  remove,
+  approve,
+  distinguish,
+  setStickyComment,
+  setSubredditSticky,
+  DISTINGUISH_TYPES,
+};
